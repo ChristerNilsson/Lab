b0a838a566cddd0b1b802ef9fe49f03e9a374516
christer@DEC2011 <janchrister.nilsson@gmail.com>
2016-02-20 22:49:46 +0100

    assert => ass

--- 2016/002-water-3-5/water exercise.py
+++ 2016/002-water-3-5/water exercise.py
@@ -4,6 +4,11 @@
 # Det ska också generaliseras till godtyckliga hinkstorlekar
 # Starta med att fylla det första kärlet.
 
+def ass(a, b):
+    if a != b:
+        print "assert misslyckades: ", " Fel: ", a, "Rätt: ", b
+        assert a == b
+
 class Water():
 
     def __init__(self, a, b):
@@ -25,35 +30,37 @@ class Water():
     def explain(lst):
         return []
 
+####################################################
+
 water = Water(5,3)
-assert water.a == 5
-assert water.b == 3
+ass(water.a, 5)
+ass(water.b, 3)
 
-assert water.next_dir(5, 0, 1) == 0
-assert water.next_dir(0, 2, 0) == 1
-assert water.next_dir(2, 3, 0) == 2
+ass(water.next_dir(5, 0, 1), 0)
+ass(water.next_dir(0, 2, 0), 1)
+ass(water.next_dir(2, 3, 0), 2)
 
-assert water.next_pos(5, 0, 0) == (2, 3)
-assert water.next_pos(0, 2, 1) == (5, 2)
-assert water.next_pos(2, 3, 2) == (2, 0)
+ass(water.next_pos(5, 0, 0), (2, 3))
+ass(water.next_pos(0, 2, 1), (5, 2))
+ass(water.next_pos(2, 3, 2), (2, 0))
 
-assert water.explain([[5, 0, 0]]) == ['a to b']
-assert water.explain([[0, 0, 1]]) == ['fill a']
-assert water.explain([[2, 3, 2]]) == ['clear b']
+ass(water.explain([[5, 0, 0]]), ['a to b'])
+ass(water.explain([[0, 0, 1]]), ['fill a'])
+ass(water.explain([[2, 3, 2]]), ['clear b'])
 
 water35 = Water(3,5)
 water53 = Water(5,3)
-assert water35.solve(1) == [[0, 0, 1], [3, 0, 0], [0, 3, 1], [3, 3, 0], [1, 5, 2]]
-assert water53.solve(1) == [[0, 0, 1], [5, 0, 0], [2, 3, 2], [2, 0, 0], [0, 2, 1], [5, 2, 0], [4, 3, 2], [4, 0, 0], [1, 3, 2]]
+ass(water35.solve(1), [[0, 0, 1], [3, 0, 0], [0, 3, 1], [3, 3, 0], [1, 5, 2]])
+ass(water53.solve(1), [[0, 0, 1], [5, 0, 0], [2, 3, 2], [2, 0, 0], [0, 2, 1], [5, 2, 0], [4, 3, 2], [4, 0, 0], [1, 3, 2]])
 
-assert water35.solve(6) == [[0, 0, 1], [3, 0, 0], [0, 3, 1]]
-assert water53.solve(6) == [[0, 0, 1], [5, 0, 0], [2, 3, 2], [2, 0, 0], [0, 2, 1], [5, 2, 0], [4, 3, 2], [4, 0, 0], [1, 3, 2], [1, 0, 0], [0, 1, 1]]
-assert Water.explain(water53.solve(6)) == ['fill a', 'a to b', 'clear b', 'a to b', 'fill a', 'a to b', 'clear b', 'a to b', 'clear b', 'a to b', 'fill a']
+ass(water35.solve(6), [[0, 0, 1], [3, 0, 0], [0, 3, 1]])
+ass(water53.solve(6), [[0, 0, 1], [5, 0, 0], [2, 3, 2], [2, 0, 0], [0, 2, 1], [5, 2, 0], [4, 3, 2], [4, 0, 0], [1, 3, 2], [1, 0, 0], [0, 1, 1]])
+ass(Water.explain(water53.solve(6)), ['fill a', 'a to b', 'clear b', 'a to b', 'fill a', 'a to b', 'clear b', 'a to b', 'clear b', 'a to b', 'fill a'])
 
 water711 = Water(7,11)
 water117 = Water(11,7)
-assert water711.solve(14) == [[0, 0, 1], [7, 0, 0], [0, 7, 1]]
-assert len(water117.solve(14)) == 31
+ass(water711.solve(14), [[0, 0, 1], [7, 0, 0], [0, 7, 1]])
+ass(len(water117.solve(14)), 31)
 
-assert water53.find_best(1) == [[0, 0, 1], [3, 0, 0], [0, 3, 1], [3, 3, 0], [1, 5, 2]]
-assert water117.find_best(14) == [[0, 0, 1], [7, 0, 0], [0, 7, 1]]
+ass(water53.find_best(1), [[0, 0, 1], [3, 0, 0], [0, 3, 1], [3, 3, 0], [1, 5, 2]])
+ass(water117.find_best(14), [[0, 0, 1], [7, 0, 0], [0, 7, 1]])
