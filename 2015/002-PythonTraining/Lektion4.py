331ddda6d72e40caa2fc22cadba0db0818cede80
christer@DEC2011 <janchrister.nilsson@gmail.com>
2016-02-21 22:24:48 +0100

    Lade till en dictionary till.

--- 2015/002-PythonTraining/Lektion4.py
+++ 2015/002-PythonTraining/Lektion4.py
@@ -29,28 +29,35 @@ assert b == {'name': 'Jenny'}
 
 ##########################################################################
 
+a = {'name': 'Mona', 'kids': ['Alfa', 'Beta']}
 b = {'name': 'Jenny', 'kids': ['Viktor', 'Ivar', 'Harald']}
 
 
 def f(x): return 0
+ass(f(a), 2)
 ass(f(b), 2)
 
 
 def g(x): return ''
+ass(g(a), 'Mona')
 ass(g(b), 'Jenny')
 
 
 def h(x): return []
+ass(h(a), ['Alfa', 'Beta'])
 ass(h(b), ['Viktor','Ivar','Harald'])
 
 
 def i(x): return 0
+ass(i(a), 2)
 ass(i(b), 3)
 
 
 def j(x): return []
+ass(j(a), ['kids', 'name'])
 ass(j(b), ['kids', 'name'])
 
 
 def k(x): return []
-ass(k(b), [['Viktor','Ivar','Harald'],'Jenny'])
+ass(k(a), [['Alfa', 'Beta'], 'Mona'])
+ass(k(b), [['Viktor', 'Ivar', 'Harald'], 'Jenny'])
