5d86204545fe697b9b4af4b97663c9b103738ccd
christer@DEC2011 <janchrister.nilsson@gmail.com>
2016-04-29 21:55:54 +0200

    NumberGuess

--- 2016/018-NumberGuess/number_guess.py
+++ 2016/018-NumberGuess/number_guess.py
@@ -2,7 +2,7 @@
 
 import random
 
-a,b=1,256
+a,b = 1,256
 
 print "Talen varierar mellan {0} och {1}".format(a,b)
 facit = random.randint(a,b)
@@ -12,7 +12,7 @@ while gissning != facit:
     antal += 1
     gissning = input("Gissa ett tal! ")
     if gissning < facit:
-        print "Ditt tal är för litet"
+        print "Ditt tal {0} är för litet".format(gissning)
     if gissning > facit:
-        print "Ditt tal är för stort"
+        print "Ditt tal {0} är för stort".format(gissning)
 print "Du gissade rätt! Det tog {0} gånger".format(antal)
