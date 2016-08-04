db65c91a9fb2ffacdaee2e7d58cd777d6377866d
christer@DEC2011 <janchrister.nilsson@gmail.com>
2016-08-05 01:43:47 +0200

    Läser nu datastruktur kalaha och janjila definierat Visar två färger

--- 2016/042-janjila/sketch.js
+++ 2016/042-janjila/sketch.js
@@ -1,7 +1,47 @@
+// Avgränsningar i 0.1 ===============
+// connections är enkelriktade
+// max två colors
+// pit ownership avgörs av norr eller söder om y=500
+// acc ownership avgörs av väster eller öster om x=500
+
+// TODO ==============================
+// Spara json
+// vända på connections. två färger.
+// connection delete 
+// connection color change med högerklick
+// Ska ej gå att ta bort pit med connections
+// Slumpad AI i egen fil. Skapa gränssnittet.
+// Riktig AI
+
+// Framtid ===========================
+// Grön zon, ägd av båda spelarna
+// Dubbelriktade connections
+// Regel: Connection skiftar färg då den används.
+// Regel: Kula skiftar färg då den läggs
+// Regel: Variationer på stöld
+//        Ingen stöld
+//        Stöld om sista pit innehåller två eller tre
+//        Stöld om sista pit är tom
+// Regel: Vad händer med stenarna då man inte kan flytta?
+
+PIT = 0
+CONN = 1
+STEAL = 2
+DEL = 3
+SEL = 4
+PLAY = 5
+UNDO = 6
+
+ACC_TYPE = 0
+PIT_TYPE = 1
+
+PIT_RADIUS = 50
+ACC_RADIUS = 65
+
 var colSlider
 var sliders = []
 var buttons = [[]]
-var mode = 1
+var mode = PIT
 var pits = []  // bara Pits 
 var selected = -1
 var mymouseX = 0 
@@ -10,48 +50,80 @@ var hand = []
 var hist = []    // list of pit indexes
 var turn = 0     // 0 = Human, 1 = Computer
 var legals = []  // list of possible moves
+var colors = []
 
-function Pit(x,y,stones,radius) {
+kalaha = { 
+    stones : [4,0],
+    pits : [
+      {type:0, x:900, y:500, stones:[0,0], neighbours:[2], colors:[0]},  // 0
+      {type:0, x:150, y:500, stones:[0,0], neighbours:[8], colors:[0]},  // 1
+      
+      {type:1, x:900, y:350, stones:[4,0], neighbours:[3], colors:[0]},  // 2
+      {type:1, x:750, y:350, stones:[4,0], neighbours:[4], colors:[0]},  // 3
+      {type:1, x:600, y:350, stones:[4,0], neighbours:[5], colors:[0]},  // 4
+      {type:1, x:450, y:350, stones:[4,0], neighbours:[6], colors:[0]},  // 5
+      {type:1, x:300, y:350, stones:[4,0], neighbours:[7], colors:[0]},  // 6
+      {type:1, x:150, y:350, stones:[4,0], neighbours:[1], colors:[0]},  // 7
+      
+      {type:1, x:150, y:650, stones:[4,0], neighbours:[9], colors:[0]},  // 8
+      {type:1, x:300, y:650, stones:[4,0], neighbours:[10], colors:[0]}, // 9
+      {type:1, x:450, y:650, stones:[4,0], neighbours:[11], colors:[0]}, // 10
+      {type:1, x:600, y:650, stones:[4,0], neighbours:[12], colors:[0]}, // 11
+      {type:1, x:750, y:650, stones:[4,0], neighbours:[13], colors:[0]}, // 12
+      {type:1, x:900, y:650, stones:[4,0], neighbours:[0], colors:[0]},  // 13
+      ],
+  }
+
+jinjala = { 
+    stones : [2,2],
+    pits : [
+      {type:0, x:950, y:500, stones:[0,0], neighbours:[2,3],   colors:[0,1]}, // 0
+      {type:0, x: 50, y:500, stones:[0,0], neighbours:[9,10],  colors:[0,1]}, // 1
+    
+      {type:1, x:950, y:200, stones:[2,2], neighbours:[3,4],   colors:[0,1]}, // 2
+      {type:1, x:800, y:350, stones:[2,2], neighbours:[4,5],   colors:[0,1]}, // 3
+      {type:1, x:650, y:200, stones:[2,2], neighbours:[5,6],   colors:[0,1]}, // 4
+      {type:1, x:500, y:350, stones:[2,2], neighbours:[6,7],   colors:[0,1]}, // 5
+      {type:1, x:350, y:200, stones:[2,2], neighbours:[7,8],   colors:[0,1]}, // 6
+      {type:1, x:200, y:350, stones:[2,2], neighbours:[1,8,10],colors:[0,1,1]}, // 7
+      {type:1, x: 50, y:200, stones:[2,2], neighbours:[1],     colors:[0]},   // 8
+
+      {type:1, x: 50, y:800, stones:[2,2], neighbours:[10,11], colors:[0,1]}, // 9
+      {type:1, x:200, y:650, stones:[2,2], neighbours:[11,12], colors:[0,1]}, // 10
+      {type:1, x:350, y:800, stones:[2,2], neighbours:[12,13], colors:[0,1]}, // 11
+      {type:1, x:500, y:650, stones:[2,2], neighbours:[13,14], colors:[0,1]}, // 12
+      {type:1, x:650, y:800, stones:[2,2], neighbours:[14,15], colors:[0,1]}, // 13
+      {type:1, x:800, y:650, stones:[2,2], neighbours:[0,3,15],colors:[0,0,1]},// 14
+      {type:1, x:950, y:800, stones:[2,2], neighbours:[0],     colors:[0]},   // 15
+      ],
+  }
+
+function Pit(x,y,stones,type) {
   this.x = x
   this.y = y
-  this.radius = radius
-  this.stones = stones  // list of colors
-  this.neighbours = []
-  this.colors = []
+  this.type = type
+  this.radius = this.type == PIT_TYPE ? PIT_RADIUS : ACC_RADIUS
+  this.stones = stones  // list of stones, in color order
+  this.neighbours = []  // index of pits reachable from this pit
+  this.colors = []      // colors of connections
   this.owner = function() {
-    if (this.radius == 50) {
+    if (this.type == PIT_TYPE) {
       return (this.y < 3*height/5) + 2*(this.y > 2*height/5)
     } else {
       return (this.x < 200+(width-200)/2) + 2*(this.x > 200+(width-200)/2)
     }
   }
   this.accumulator = function() {
-    return this.radius != 50
+    return this.radius != ACC_RADIUS
   }
 }
 
-function pit() { // [mode, x, y, [color1, color2], radius]
-  mode = 1
-}
-function conn() {
-  mode = 2
-}
-function steal() {
-  mode = 3
-}
-function acc() {
-  mode = 4
-}
-function del() {
-  mode = 5
-}
-function sel() {
-  mode = 6
-}
-function play() {
-  mode = 7
-  hand[0] = 0
-}
+function pit() { mode = PIT }
+function conn() { mode = CONN }
+function steal() { mode = STEAL }
+function del() { mode = DEL }
+function sel() { mode = SEL }
+function play() { mode = PLAY; hand[0] = 0 }
 
 function undo() {
   if (hist.length > 2) {
@@ -77,25 +149,18 @@ function undo() {
 
 function pickUp() {
   var pit = pits[selected]
-  if (pit == null)
-    return
+  if (pit == null) return
   hand = pit.stones
-  pit.stones = [0]
-  //console.log("pickUp")
-  //console.log(hist)
+  pit.stones = [0,0]
   hist.push(selected)
-  //console.log(hand)
 }
 
 function lay() {
-  //console.log("lay")
-  //console.log(hand)
   if (hand[0] > 0) {
     pits[selected].stones[0]++
     hand[0]--
     hist.push(selected)
-    if (hand[0] == 0)
-      hist = []
+    if (hand[0] == 0) hist = []
   }
 }
 
@@ -136,42 +201,64 @@ function makeSlider(x,y,mini,maxi,val) {
 }
 
 function setup() {
+  //data = kalaha
+  data = jinjala
+
+  colors.push(color(255,0,0))
+  colors.push(color(0,255,0))
+  colors.push(color(0,0,255))
+  colors.push(color(255,255,0))
+  colors.push(color(255,0,255))
+  colors.push(color(0,255,255))
+  
   createCanvas(1000, 1000)
   background(128)
   textSize(15)
 
-  // sliders
-  x = 20
+  y = 20
+  buttons.push(makeButton(50,y,'Pit',pit))
+  buttons.push(makeButton(100,y,'Conn',conn))
+  buttons.push(makeButton(150,y,'Steal',steal))
+  buttons.push(makeButton(200,y,'Del',del))
+  buttons.push(makeButton(250,y,'Sel',sel))
+  buttons.push(makeButton(300,y,'Play',play))
+  buttons.push(makeButton(350,y,'Undo',undo))
   
-  buttons.push(makeButton(x,20,'Pit',pit))
-  buttons.push(makeButton(x,50,'Conn',conn))
-  buttons.push(makeButton(x,80,'Steal',steal))
-//  buttons.push(makeButton(x,290,'Acc',acc))
-  buttons.push(makeButton(x,110,'Del',del))
-  buttons.push(makeButton(x,140,'Sel',sel))
-  buttons.push(makeButton(x,170,'Play',play))
-  buttons.push(makeButton(x,200,'Undo',undo))
-  
-  colSlider = makeSlider(x,230,1,6,1)
+  colSlider = makeSlider(400,y,1,2,data.stones.length)
   sliders = []
-
-  for (var i = 0; i<6; i++) {
-    sliders.push(makeSlider(x,260+i*30,0,10,4))
+  for (var i=0; i<2; i++) {
+    sliders.push(makeSlider(600+i*200,y,0,10,4))
   }
 
   colSlider.input(changeSliders)
   changeSliders()
   
-  pits.push(new Pit(240,height/2,[0],60))
-  pits.push(new Pit(width-40,height/2,[0],60))
+  // Accumulators
+  //pits.push(new Pit(240,height/2,[0,0],ACC_RADIUS))
+  //pits.push(new Pit(width-40,height/2,[0,0],ACC_RADIUS))
+  
+  for (var i=0; i<data.pits.length; i++) {
+    var dp = data.pits[i]
+    p = new Pit(dp.x, dp.y, dp.stones, dp.type)
+    p.neighbours = dp.neighbours
+    p.colors = dp.colors
+    pits.push(p)
+  }
 }
 
-function changeSliders() { // hide/show
-//  console.log(colSlider)
-  for (var i = 0; i<6; i++) {
+function changeSliders() { 
+  for (var i = 0; i<sliders.length; i++) {
     slider = sliders[i]
-    slider.position(i<colSlider.value() ? 20 : -1000, slider.y)
+    slider.position(slider.x, i<colSlider.value() ? 20 : -100)
   }
+}
+
+function sliderList() {
+  res = []
+  for (var i = 0; i < sliders.length; i++) {
+    res.push(sliders[i].value())
+  }
+  return res
 }
 
 function dist(x1,y1,x2,y2) {
@@ -196,7 +283,7 @@ function find(x,y) {
 
 function delPit() {
   pit = pits[selected]
-  if (pit.radius == 50) {
+  if (pit.radius == PIT_RADIUS) {
     pits[selected] = null
     selected = -1
   }
@@ -206,65 +293,63 @@ function mousePressed() {
   mymouseX = winMouseX
   mymouseY = winMouseY
 
-  if (mouseX<200) return
-  if (mode == 1) { // Create Pit
-//    console.log("mousePressed")
-    pits.push(new Pit(mouseX,mouseY,[sliders[0].value()],50))
-//    console.log(pits)    
+  if (mouseY<100) return
+  if (mode == PIT) { 
+    pits.push(new Pit(mouseX,mouseY,sliderList(),PIT_TYPE))
   } else if (mode == 22) {
     selected2 = find(mouseX,mouseY)
     if (selected2 != -1) pits[selected].neighbours.push(selected2)
-    mode = 2
-  } else if (mode == 2) {
+    mode = CONN
+  } else if (mode == CONN) {
     selected = find(mouseX,mouseY)
     if (selected != -1) mode = 22
-  } else if (mode == 4) { // Create Accumulator
-    pits.push(new Pit(mouseX,mouseY,[0],60))
-  } else if (mode == 5) {  // Delete
+  } else if (mode == DEL) {  // Delete
     selected = find(mouseX,mouseY)
     if (selected != -1) delPit(selected)
-  } else if (mode == 6) {
+  } else if (mode == SEL) {
     selected = find(mouseX,mouseY)
-  } else if (mode == 7) {
+  } else if (mode == PLAY) {
     selected = find(mouseX,mouseY)
-    if (hand[0] == 0)
-      pickUp()
-    else
-      lay()
+    if (hand == [0,0]) { pickUp() } else lay()
   }
 }
 
 function drawpit(n) {
-  if (n==selected) {
-    stroke(255)
-  } else {
-    stroke(0)
-  }
+  stroke(n==selected ? 255 : 0)
   pit = pits[n]
+  
+  // markera möjligt drag
   if (legals.indexOf(n) != -1) {
     noFill()
-    ellipse(pit.x,pit.y,pit.radius + 5,pit.radius+5)
+    ellipse(pit.x, pit.y, pit.radius+5, pit.radius+5)
   }
+  
+  // rita själva piten
   fill(0)
-  ellipse(pit.x,pit.y,pit.radius,pit.radius)
-  noStroke(); fill(255,0,0); textAlign(CENTER,CENTER); text(pit.stones[0], pit.x, pit.y)
+  ellipse(pit.x, pit.y, pit.radius, pit.radius)
+  
+  // visa antal stenar
+  noStroke(); textAlign(CENTER,CENTER); 
+  if (colSlider.value()==1) {
+    fill(colors[0]); text(pit.stones[0], pit.x, pit.y)
+  } else if (colSlider.value()==2) {
+    fill(colors[0]); text(pit.stones[0], pit.x, pit.y-8)
+    fill(colors[1]); text(pit.stones[1], pit.x, pit.y+8)
+  }
 }
 
+// Rita riktning för connection
 function drawdirection(n) {
-  if (n==selected) {
-    stroke(255)
-  } else {
-    stroke(0)
-  }
+  stroke(n==selected ? 255 : 0)
   pit = pits[n]
   for (var i=0; i<pit.neighbours.length; i++) {
     var nb = pit.neighbours[i]
-    p1 = pit //lst[item[1]]
+    p1 = pit 
     p2 = pits[nb]
     if (p2) {
       d = dist (p1.x,p1.y,p2.x,p2.y)
-      x = map(25,0,d,p1.x,p2.x)
-      y = map(25,0,d,p1.y,p2.y)
+      x = map(pit.radius/2,0,d,p1.x,p2.x)
+      y = map(pit.radius/2,0,d,p1.y,p2.y)
       stroke(0)
       ellipse(x,y,10,10)
     }
@@ -272,19 +357,15 @@ function drawdirection(n) {
 }
 
 function drawconnection(n) {
-  if (n==selected) {
-    stroke(255)
-  } else {
-    stroke(0)
-  }
-  //console.log(pits)
-  pit = pits[n]
+  stroke(n==selected ? 255 : 0)
+  var pit = pits[n]
   for (var i=0; i<pit.neighbours.length; i++) {
     var nb = pit.neighbours[i]
-    p1 = pit //lst[item[1]]
-    p2 = pits[nb]
+    var p1 = pit 
+    var p2 = pits[nb]
     if (p2) {
-      stroke (255)
+      //console.log(pit.colors[i])
+      stroke(colors[pit.colors[i]])  
       line(p1.x,p1.y,p2.x,p2.y)
     }
   }
@@ -294,29 +375,37 @@ function drawHand() {
   noStroke(); fill(255,255,0); textAlign(CENTER,CENTER); text(hand, 20, 700)
 }
 
+function drawTexts() {
+  noStroke(); 
+  textAlign(CENTER,CENTER); 
+  for (var i=0; i<sliders.length; i++) {
+    var slider = sliders[i]
+    fill(colors[i]); text(slider.value(), slider.x+slider.width+20, slider.y+12)
+  }
+}
+
 function draw() {
   background(128)
-  fill(40,120,40)
-  noStroke()
   
-  rect(200,2*height/5,width,height/5)
+  // green neutral zone, owned by both players
+  //fill(40,120,40); noStroke(); rect(200,2*height/5,width,height/5)
+  line(0,500,1000,500)
   
-  stroke(0)
-  fill(0)
-  ellipse(10,0+30*mode,10,10)
+  // indicate mode with small black circle
+  stroke(0); fill(0); line(50*(mode+1),50,50*(mode+2),50)
   
   legals = legalMoves()
-  //console.log(legals)
 
   for (var i=0; i<pits.length; i++) if (pits[i]!= null) drawconnection(i)
   for (var i=0; i<pits.length; i++) if (pits[i]!= null) drawdirection(i)
   for (var i=0; i<pits.length; i++) if (pits[i]!= null) drawpit(i)
   drawHand()
+  drawTexts()
 }
 
 function mouseDragged(event) {
   // selected anger pit
-  if (mode==6 && selected>=0) {
+  if (mode==SEL && selected>=0) {
     var dx = winMouseX - mymouseX 
     var dy = winMouseY - mymouseY
     mymouseX = winMouseX
