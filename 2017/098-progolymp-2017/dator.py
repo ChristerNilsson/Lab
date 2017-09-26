3b7fd248bc0e0908a03b71c84f735cb446834535
ChristerNilsson <janchrister.nilsson@gmail.com>
2017-09-26 02:05:33 +0200

    x

--- 2017/098-progolymp-2017/dator.py
+++ 2017/098-progolymp-2017/dator.py
@@ -1,75 +1,60 @@
-import random
+#import time
 import copy
 avd = []
 TOTAL = 0
-EXPENSIVE = 1
-CHEAP = 2
-price = [0,0,0]
+CHEAP = 1
+EXPENSIVE = 2
 
-def better(rich,poor,level):
-    if avd[rich][level]==0: return False
-    return min(avd[poor][TOTAL]+price[level], avd[rich][TOTAL]-price[level]) > avd[poor][TOTAL]
+#optima = [[900,3,0],[800,1,1],[600,2,0]]
+#optima = [[330,3,0],[300,0,2],[260,1,1]]
+optima = [[2330,2,5], [2258,6,2], [2221,3,4], [2184,0,6], [2149,7,1], [2112,4,3], [2075,1,5], [2040,8,0], [2003,5,2] ]
 
-def swap(rich,poor):
-    if avd[rich][EXPENSIVE]==0: return False
-    if avd[poor][CHEAP]==0: return False
-    return min(avd[poor][TOTAL]+price[EXPENSIVE]- price[CHEAP], avd[rich][TOTAL]-price[EXPENSIVE]+ price[CHEAP]) > avd[poor][TOTAL]
-
-def transfer(rich,poor,level):
+def optimize():
     global avd
-    avd[rich][TOTAL] -= price[level]
-    avd[rich][level] -= 1
-    avd[poor][TOTAL] += price[level]
-    avd[poor][level] += 1
+    for optimum in optima:
+        [t1,x1,y1] = optimum
+        [t2,x2,y2] = avd[0]
+        [dx,dy] = [x1-x2,y1-y2]
+        for i in reversed(range(n)):
+            [t3,x3,y3] = avd[i]
+            [x4,y4] = [x3-dx,y3-dy]
+            if x4>=0 and y4>=0: # make a move
+                belopp = a*x4+b*y4
+                if t2 != belopp and belopp > avd[0][TOTAL] and t1 != t2 and t1 > avd[0][TOTAL]:
+                    avd[0] = [a*x1+b*y1,x1,y1]
+                    avd[i] = [belopp,x4,y4]
+                    #print dx,dy,x4,y4
+                    #print 'swap',t2,t1
+                    #print 'swap',t3,belopp
+                    return
+    #print 'no more optimas'
 
 def f(x,a,y,b,n):
     global avd,price
-    price[EXPENSIVE] = a
-    price[CHEAP] = b
     old = []
     while str(old) != str(avd):
         old = copy.deepcopy(avd)
-        for i in reversed(range(1,n)):
-            if better(i,0,EXPENSIVE):
-                transfer(i,0,EXPENSIVE)
-            elif better(i,0,CHEAP):
-                transfer(i,0,CHEAP)
-            elif swap(i,0):
-                transfer(i,0,EXPENSIVE)
-                transfer(0,i,CHEAP)
-        for i in range(n-1):
-            if better(n-1,i,EXPENSIVE):
-                transfer(n-1,i,EXPENSIVE)
-            elif better(n-1,i,CHEAP):
-                transfer(n-1,i,CHEAP)
-            elif swap(n-1,i):
-                transfer(n-1,i,EXPENSIVE)
-                transfer(i,n-1,CHEAP)
+        optimize()
         avd = sorted(avd)
-        print avd
+        #print avd
     return avd[0][TOTAL]
 
-def init():
+def init(x,a,y,b,n):
     global avd
     avd = []
     for i in range(n):
         avd.append([0,0,0]) # [TOTAL,EXPENSIVE,CHEAP]
-    avd[n-1] = [x*a + y*b, x, y]
-
-def initRandom(x,a,y,b,n):
-    global avd
-    avd = []
-    for i in range(n):
-        avd.append([0,0,0]) # [TOTAL,EXPENSIVE,CHEAP]
-    for i in range(x):
-        index = random.randint(0,n-1)
-        avd[index][TOTAL] += a
-        avd[index][EXPENSIVE] += 1
+    index = 0
     for i in range(y):
-        index = random.randint(0,n-1)
-        avd[index][TOTAL] += b
-        avd[index][CHEAP] += 1
+        avd[index%n][TOTAL] += b
+        avd[index%n][EXPENSIVE] += 1
+        index+=1
+    for i in range(x):
+        avd[index%n][TOTAL] += a
+        avd[index%n][CHEAP] += 1
+        index+=1
     avd = sorted(avd)
+    #print avd
 
 import sys
 for i in sys.stdin:
@@ -80,10 +65,15 @@ for i in sys.stdin:
     b = int(ab[3])
     n = int(ab[4])
 
-#while True:
+    if a>b:
+        [a,b] = [b,a]
+        [x,y] = [y,x]
 
-#init()
-initRandom(x,a,y,b,n)
-res = f(x,a,y,b,n)
-#if res != 2330:
-#    break
+#x,a,y,b,n = 2,500,3,300,2
+#x,a,y,b,n = 3,150,5,110,3
+#x,a,y,b,n = 37,255,58,364,13
+
+#start = time.time()
+init(x,a,y,b,n)
+print f(x,a,y,b,n)
+#print time.time() - start
