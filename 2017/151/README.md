df8cbe947d88fb2ef6de506736a0c7594f8b2667
ChristerNilsson <janchrister.nilsson@gmail.com>
2017-12-04 22:56:53 +0100

    x

---
+++ 2017/151/README.md
@@ -0,0 +1,11 @@
+# 151
+
+react + qr + shortcut
+
+npm start
+
+yarn build
+
+npm install gh-pages
+
+yarn run deploy
---
+++ 2017/151/backup/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import QrReader from 'react-qr-reader'
+
+class App extends Component {
+constructor(props){
+    super(props)
+    this.state = {
+      delay: 500,
+      result: 'No result',
+    }
+
+    this.handleScan = this.handleScan.bind(this)
+  }
+  handleScan(result){
+    if(result){
+      console.log(result)
+      this.setState({ result })
+    }
+  }
+  handleError(err){
+    console.error(err)
+  }
+  render(){
+    const previewStyle = {
+      height: 240,
+      width: 320,
+    }
+
+    return(
+      <div>
+        <p>{this.state.result}</p>
+        <QrReader
+          delay={this.state.delay}
+          style={previewStyle}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          />
+        <p>{this.state.result}</p>
+      </div>
+    )
+  }
+}
+
+export default App;
