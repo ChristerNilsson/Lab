7cd3dc257021bb0b03eddc7640c8fef2cf1ed465
ChristerNilsson <janchrister.nilsson@gmail.com>
2017-12-07 02:50:31 +0100

    Ten languages

--- 2017/136-Lettero/136.py
+++ 2017/136-Lettero/136.py
@@ -1,15 +1,18 @@
-with open('ordlista.txt','r',encoding='utf-8') as f:
-	words = f.readlines()
+for language in 'swe eng fra dan ger isl ita nor rus spa'.split(' '):
 
-with open('swe4-15.js','w',encoding='utf-8') as g:
-	res = []
-	for word in words:
-		w = word.strip()
-		arr = w.split('/')
-		if len(arr)==2:
-			w = arr[0]
+	with open('data/' + language + '.txt','r',encoding='utf-8') as f:
+		words = f.readlines()
+		print(language)
+
+	with open('data/' + language +'.js','w',encoding='utf-8') as g:
+		res = []
+		for word in words:
+			w = word.strip()
+			arr = w.split('/')
+			if len(arr)==2: w = arr[0]
 			if '.' not in w:
 				if 4 <= len(w) <= 15:
 					if w.lower() == w:
 						res.append(w)
-	g.write('ordlista="' +  ' '.join(res) + '"')
+		g.write('ordlista="' +  ' '.join(res) + '"')
+		print(language,len(res))
