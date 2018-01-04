e00117425c2d820484ca265c71d501a05632e77a
ChristerNilsson <janchrister.nilsson@gmail.com>
2018-01-04 14:34:50 +0100

    x

--- 2018/001-SushiRestaurant/coffee/sketch.coffee
+++ 2018/001-SushiRestaurant/coffee/sketch.coffee
@@ -2,8 +2,9 @@ setup = ->
 	createCanvas 400,400
 
 draw = ->
-	bg 1,1,0
+	bg 1,1,1
 
 mousePressed = ->
 	#window.location.href = "sms:+46707496800&body=message" # iOS ok!
-	window.location.href = "sms://+46707496800?&body=message" # 
+	#window.location.href = "sms://+46707496800?&body=message" # iOS ok
+	window.location = "sms://+46707496800" # 
--- 2018/001-SushiRestaurant/js/sketch.js
+++ 2018/001-SushiRestaurant/js/sketch.js
@@ -8,11 +8,12 @@ setup = function setup() {
 };
 
 draw = function draw() {
-  return bg(1, 1, 0);
+  return bg(1, 1, 1);
 };
 
 mousePressed = function mousePressed() {
   //window.location.href = "sms:+46707496800&body=message" # iOS ok!
-  return window.location.href = "sms://+46707496800?&body=message";
+  //window.location.href = "sms://+46707496800?&body=message" # iOS ok
+  return window.location = "sms://+46707496800";
 };
 //# sourceMappingURL=sketch.js.map
