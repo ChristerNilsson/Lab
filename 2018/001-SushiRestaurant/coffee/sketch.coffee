58e5706f7cf61a18a69c7760a0a7de6d0858c902
ChristerNilsson <janchrister.nilsson@gmail.com>
2018-01-04 14:21:23 +0100

    x

--- 2018/001-SushiRestaurant/coffee/sketch.coffee
+++ 2018/001-SushiRestaurant/coffee/sketch.coffee
@@ -2,9 +2,8 @@ setup = ->
 	createCanvas 400,400
 
 draw = ->
-	bg 1,0,0
+	bg 1,0,1
 
 mousePressed = ->
-	#window.location.href = "sms://+46707496800;?&body=message%20more%20message"
-	window.location.href = "sms:+46707496800&body=message"
-	#window.open 'sms:+46707496800?body=SUBSCRIBE' # , '_self'	
+	#window.location.href = "sms:+46707496800&body=message" # iOS ok!
+	window.location.href = "sms:+46707496800?&body=message" # 
--- 2018/001-SushiRestaurant/js/sketch.js
+++ 2018/001-SushiRestaurant/js/sketch.js
@@ -8,13 +8,11 @@ setup = function setup() {
 };
 
 draw = function draw() {
-  return bg(1, 0, 0);
+  return bg(1, 0, 1);
 };
 
 mousePressed = function mousePressed() {
-  //window.location.href = "sms://+46707496800;?&body=message%20more%20message"
-  return window.location.href = "sms:+46707496800&body=message";
+  //window.location.href = "sms:+46707496800&body=message" # iOS ok!
+  return window.location.href = "sms:+46707496800?&body=message";
 };
-
-//window.open 'sms:+46707496800?body=SUBSCRIBE' # , '_self'
 //# sourceMappingURL=sketch.js.map
