bf8d065ec012ef19a78d7cf5c18909b1babfed4f
ChristerNilsson <janchrister.nilsson@gmail.com>
2018-01-21 14:19:08 +0100

    x

--- 2018/005-MobileGUI/js/sketch.js
+++ 2018/005-MobileGUI/js/sketch.js
@@ -164,8 +164,8 @@ locationUpdate = function locationUpdate(position) {
     totalTime = calcTotalTime(start, millis(), track[0], _.last(track), place());
     texts[3] = precisionRound(3.6 * speed, 1) + ' km/h';
     texts[2] = prettyETA(startDate, totalTime);
+    return logg.push("yxa"); // #{p1.timestamp} #{p1.lat} #{p1.lng} #{heading_12} #{speed} #{totaltime}"
   }
-  return logg.push(p1.timestamp + ' ' + p1.lat + ' ' + p1.lng + ' ' + heading_12 + ' ' + speed + ' ' + totaltime);
 };
 
 locationUpdateFail = function locationUpdateFail(error) {};
