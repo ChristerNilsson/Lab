d52213966a905ad0713d17251775cb158bc1a212
ChristerNilsson <janchrister.nilsson@gmail.com>
2018-05-04 23:08:34 +0200

    x

--- 2018/028-TaiwaneseRemainder/028B.py
+++ 2018/028-TaiwaneseRemainder/028B.py
@@ -1,6 +1,10 @@
-# startar man med jamnt fordelad antalslista funkar denna rutin bra och snabbt.
-# Start med alla mynt av samma valor ger ofta loopar, typ 3028, 3026, 3028...
-# Myntens valorer ligger i listan ticks.
+# -*- coding: utf-8 -*-
+
+import random
+
+# Startar man med jämnt fördelad antalslista funkar denna rutin bra och snabbt.
+# Start med alla mynt av samma valör ger ofta loopar, typ 3028, 3026, 3028...
+# Myntens valörer ligger i listan ticks.
 
 def prod(ticks,antal): return reduce(lambda a,b: a+b, [t*a for t,a in zip(ticks,antal)])
 
@@ -12,25 +16,40 @@ def bestSwap(ticks,sum,antal):
 		if antal[i] == 0: continue
 		for j in range(n):
 			if i == j: continue
-			diff = abs(s - ticks[i] + ticks[j] - sum)
-			if diff < best[0]: best = [diff,i,j]
+			newsum = s-ticks[i]+ticks[j]
+			diff = abs(newsum - sum)
+			if diff < best[0]: best = [diff,i,j,newsum]
 	return best
 
-# Swappar sa lange forbattringar sker.
+# Swappar så länge förbättringar sker.
 def solve(ticks, sum, steps):
-	antal = [0] * len(ticks) # sprid ut jamnt
+	antal = [0] * len(ticks) # sprid ut jämnt
 	for i in range(steps): antal[i % len(antal)] += 1
 	diff = 999999
 	while diff != 0:
-		diff,i,j = bestSwap(ticks,sum,antal)
+		diff,i,j,newsum = bestSwap(ticks,sum,antal)
+		#print newsum,i,j,antal
 		antal[i] -= 1
 		antal[j] += 1
 	return antal
 
-assert [1,2,4] == solve([3,7,13], 69, 7)
-assert [4,4,4,3,2,3] == solve([2,3,5,13,17,19], 170, 20)
+#assert [1,2,4] == solve([3,7,13], 69, 7)
+#assert [4,4,4,3,2,3] == solve([2,3,5,13,17,19], 170, 20)
 ticks = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73]
-assert [0,0,0,0,0,1,2,1,1,1,0,0,0,0,0,0,0,0,0,0,4] == solve(ticks,410,10)
-assert [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10] == solve(ticks,730,10)
-assert [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,15] == solve(ticks,73*15,15)
-assert [7,5,5,6,5,5,5,5,5,5,5,5,5,5,5,5,4,4,3,4,2] == solve(ticks,3027,100)
+# assert [0,0,0,0,0,1,2,1,1,1,0,0,0,0,0,0,0,0,0,0,4] == solve(ticks,410,10)
+# assert [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10] == solve(ticks,730,10)
+# assert [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,15] == solve(ticks,73*15,15)
+# assert [7,5,5,6,5,5,5,5,5,5,5,5,5,5,5,5,4,4,3,4,2] == solve(ticks,3027,100)
+
+primes = [2,3,5,7,11,13] #,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173]
+ticks = primes
+#ticks = random.sample(primes, 20)
+#ticks.sort()
+print ticks
+n=10
+for i in range(ticks[0]*n,ticks[-1]*n+1):
+	print i,solve(ticks,i,n)
+
+
+
+
--- 2018/028-TaiwaneseRemainder/coffee/utils.coffee
+++ 2018/028-TaiwaneseRemainder/coffee/utils.coffee
@@ -1,12 +1,21 @@
+PRIMES = [2,3,5,7,11, 13,19,23,29,31, 37,41,43,47,53, 59,61,67,71,73, 79,83,89,97,101, 103,107,109,113,127] #,131,137,139,149,151,157,163,167,173]
+
 createRests = (ticks,total) -> (total % t for t in ticks)
 
 createProblem = (steps) ->
-	primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
-	antal = int map steps,1,125,4,25
-	ticks = _.sample primes[..antal], 2 + steps // 5
+	antalPrimtal = int map steps,1,125,4,PRIMES.length
+	antalPrimtal = constrain antalPrimtal,4,PRIMES.length
+	antalKlockor = constrain 2 + steps // 5, 2, 125
+	ticks = _.sample PRIMES[..antalPrimtal], antalKlockor
 	ticks.sort (a,b) -> a-b
-	#ticks = [3,5]
-	total = (_.sample ticks for i in range steps).reduce (a,b)->a+b
+
+	# första metoden ger alltför jämnt fördelade problem.
+	# total = (_.sample ticks for i in range steps).reduce (a,b)->a+b 
+	# Vissa totalsummor är omöjliga att uppnå. Max 50%
+	solution = null
+	while solution == null
+		total = _.random steps * ticks[0], steps * _.last ticks
+		solution = solve ticks,total,steps
 
 	rests = createRests ticks,total
 	h = window.location.href
@@ -16,6 +25,31 @@ createProblem = (steps) ->
 	print url
 
 	{steps,ticks,rests,url}
+
+solve = (ticks,sum,n) ->
+	tabell = new Array sum+1
+	tabell.fill null
+	rad = new Array ticks.length
+	rad.fill 0
+	tabell[n * ticks[0]] = rad
+	tabell[n * ticks[0]][0] = n
+	for i in range n,sum
+		rad = tabell[i]
+		if rad == null then continue
+		for j in range ticks.length
+			if rad[j] == 0 then continue
+			tj = ticks[j]
+			for k in range ticks.length
+				if k <= j then continue
+				tk = ticks[k]
+				index = i - tj + tk
+				if index > sum then continue
+				if tabell[index] == null
+					tabell[index] = rad[..]
+					tabell[index][j] -= 1
+					tabell[index][k] += 1
+	tabell[sum]
+assert [5,1,0,0,0,4], solve [2,3,5,7,11,13],65,10
 
 copyToClipboard = (s) -> 
 	el = document.createElement 'textarea'
