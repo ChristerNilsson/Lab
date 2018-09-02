4fb9a154032c6fe4b53a9908bc4864983d99417b
ChristerNilsson <janchrister.nilsson@gmail.com>
2018-09-02 23:53:08 +0200

    marked innehåller nu både kolumn och rad. Detta för att kunna hantera två klick på samma kolumn, men olika kort.

--- 2018/056-GeneralensTidsfördriv/coffee/sketch.coffee
+++ 2018/056-GeneralensTidsfördriv/coffee/sketch.coffee
@@ -367,14 +367,14 @@ prettyUndoMove = (src,dst,b,antal) ->
 
 # returns destination
 oneClick = (data,marked,board,sharp=false) ->
-	if data.lastMarked == marked then data.counter++ else data.counter = 0
+	if _.isEqual data.lastMarked, marked then data.counter++ else data.counter = 0
 
 	holes = []
 	found = false
 
 	for heap in ACES
-		if legalMove board,marked,heap  
-			if sharp then makeMove board,marked,heap,true
+		if legalMove board,marked[0],heap  
+			if sharp then makeMove board,marked[0],heap,true
 			found = true 
 			return heap
 
@@ -382,59 +382,63 @@ oneClick = (data,marked,board,sharp=false) ->
 		alternativeDsts = [] # för att kunna välja mellan flera via Undo
 		for heap in HEAPS
 			if board[heap].length == 0
-				if marked in PANEL or calcAntal(board[marked]) > 1
+				if marked[0] in PANEL or calcAntal(board[marked[0]]) > 1
 					holes.push heap
 			else 
-				if legalMove board,marked,heap
+				if legalMove board,marked[0],heap
 					alternativeDsts.push heap
 		if holes.length > 0 then alternativeDsts.push holes[0]		
 
 		if alternativeDsts.length > 0
 			heap = alternativeDsts[data.counter % alternativeDsts.length]  
-			if sharp then makeMove board,marked,heap,true
+			if sharp then makeMove board,marked[0],heap,true
 			data.lastMarked = marked 
 			return heap
 
-	return marked # no Move can happen
+	return marked[0] # no Move can happen
 
 # assert1.jpg
 b1 = readBoard "cA|hA|sA|dA|h5|c3|s65|c2 d5||s3|d2 h6 d4|d3 h4|h2|c5|c4|h3|c6|s4|s2|d6"
-assert 11, oneClick {lastMarked:0, counter:0},4,b1 # hj5 to hj4
-assert 5,  oneClick {lastMarked:0, counter:0},5,b1 # kl3 no move
-assert 8,  oneClick {lastMarked:0, counter:0},6,b1 # sp5 to hole
+assert 11, oneClick {lastMarked:0, counter:0},[4,0],b1 # hj5 to hj4
+assert 5,  oneClick {lastMarked:0, counter:0},[5,0],b1 # kl3 no move
+assert 8,  oneClick {lastMarked:0, counter:0},[6,1],b1 # sp5 to hole
 
-assert 10, oneClick {lastMarked:0, counter:0},7,b1 # ru5 to ru4
-assert 8,  oneClick {lastMarked:7, counter:0},7,b1 # ru5 to hole
+assert 10, oneClick {lastMarked:0, counter:0},[7,1],b1 # ru5 to ru4
+assert 8,  oneClick {lastMarked:[7,1], counter:0},[7,1],b1 # ru5 to hole
 
-assert 8, oneClick {lastMarked:0, counter:0},8,b1 # hole click
-assert 9, oneClick {lastMarked:0, counter:0},9,b1 # sp3 no move
+assert 8, oneClick {lastMarked:0, counter:0},[8,-1],b1 # hole click
+assert 9, oneClick {lastMarked:0, counter:0},[9,0],b1 # sp3 no move
 
-assert 7, oneClick {lastMarked:0, counter:0},10,b1 # ru4 to ru5
-assert 8, oneClick {lastMarked:10, counter:0},10,b1 # ru4 to hole
-assert 7, oneClick {lastMarked:10, counter:1},10,b1 # ru4 to ru5
+assert 7, oneClick {lastMarked:0, counter:0},[10,2],b1 # ru4 to ru5
+assert 8, oneClick {lastMarked:[10,2], counter:0},[10,2],b1 # ru4 to hole
+assert 7, oneClick {lastMarked:[10,2], counter:1},[10,2],b1 # ru4 to ru5
 
-assert 4, oneClick {lastMarked:0, counter:0},11,b1 # hj4 to hj5
-assert 8, oneClick {lastMarked:11, counter:0},11,b1 # hj4 to hole
+b1a = readBoard "cA|hA|sA|dA|h5|c3|s65|c2 d54||s3|d2 h6|d3 h4|h2|c5|c4|h3|c6|s4|s2|d6"
+assert 4, oneClick {lastMarked:[10,2], counter:0},[10,1],b1a # hj6 to hj5
+assert 8, oneClick {lastMarked:[10,1], counter:0},[10,1],b1a # hj6 to hole
 
-assert 1, oneClick {lastMarked:0, counter:0},12,b1 # hj2 to A
-assert 8, oneClick {lastMarked:0, counter:0},13,b1 # kl5 to hole
+assert 4, oneClick {lastMarked:0, counter:0},[11,1],b1 # hj4 to hj5
+assert 8, oneClick {lastMarked:[11,1], counter:0},[11,1],b1 # hj4 to hole xxx
 
-assert 5, oneClick {lastMarked:0, counter:0},14,b1 # kl4 to kl3
-assert 8, oneClick {lastMarked:14, counter:0},14,b1 # kl4 to hole
+assert 1, oneClick {lastMarked:0, counter:0},[12,0],b1 # hj2 to A
+assert 8, oneClick {lastMarked:0, counter:0},[13,0],b1 # kl5 to hole
 
-assert 11, oneClick {lastMarked:0, counter:0},15,b1 # hj3 to hj4
-assert 8, oneClick {lastMarked:15, counter:0},15,b1 # hj3 to hole
+assert 5, oneClick {lastMarked:0, counter:0},[14,0],b1 # kl4 to kl3
+assert 8, oneClick {lastMarked:[14,0], counter:0},[14,0],b1 # kl4 to hole
 
-assert 8, oneClick {lastMarked:0, counter:0},16,b1 # kl6 to hole
+assert 11, oneClick {lastMarked:0, counter:0},[15,0],b1 # hj3 to hj4
+assert 8, oneClick {lastMarked:[15,0], counter:0},[15,0],b1 # hj3 to hole
 
-assert 6, oneClick {lastMarked:0, counter:0},17,b1 # sp4 to sp5
-assert 9, oneClick {lastMarked:17, counter:0},17,b1 # sp4 to sp3
-assert 8, oneClick {lastMarked:17, counter:1},17,b1 # sp4 to hole
+assert 8, oneClick {lastMarked:0, counter:0},[16,0],b1 # kl6 to hole
 
-assert 2, oneClick {lastMarked:0, counter:0},18,b1 # sp2 to A
+assert 6, oneClick {lastMarked:0, counter:0},[17,0],b1 # sp4 to sp5
+assert 9, oneClick {lastMarked:[17,0], counter:0},[17,0],b1 # sp4 to sp3
+assert 8, oneClick {lastMarked:[17,0], counter:1},[17,0],b1 # sp4 to hole
 
-assert 7, oneClick {lastMarked:0, counter:0},19,b1 # ru6 to ru5
-assert 8, oneClick {lastMarked:19, counter:0},19,b1 # ru6 to hole
+assert 2, oneClick {lastMarked:0, counter:0},[18,0],b1 # sp2 to A
+
+assert 7, oneClick {lastMarked:0, counter:0},[19,0],b1 # ru6 to ru5
+assert 8, oneClick {lastMarked:[19,0], counter:0},[19,0],b1 # ru6 to hole
 
 # assert2.jpg
 b2 = readBoard "cA|hA|sA|dA|d5 h2 d3 h3|c7|c34|d4 h76|||s3 d6 c6|d7 c5 d2|c2|s4|s6|h5|s5|s7|s2|h4"
@@ -456,7 +460,7 @@ mousePressed = ->
 			display board
 			return
 
-		marked = mx + if my >= 3 then 12 else 4
+		marked = [(mx + if my >= 3 then 12 else 4),my]
 		heap = oneClick oneClickData,marked,board,true
 
 		if msg == '' and 4*N == countAceCards board 
--- 2018/056-GeneralensTidsfördriv/js/sketch.js
+++ 2018/056-GeneralensTidsfördriv/js/sketch.js
@@ -40,6 +40,7 @@ var ACES,
     aceCards,
     assert,
     b1,
+    b1a,
     b2,
     backs,
     board,
@@ -739,8 +740,8 @@ prettyUndoMove = function prettyUndoMove(src, dst, b, antal) {
 oneClick = function oneClick(data, marked, board) {
   var sharp = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : false;
 
-  var alternativeDsts, found, heap, holes, j, l, len, len1;
-  if (data.lastMarked === marked) {
+  var alternativeDsts, found, heap, holes, j, l, len, len1, ref;
+  if (_.isEqual(data.lastMarked, marked)) {
     data.counter++;
   } else {
     data.counter = 0;
@@ -749,9 +750,9 @@ oneClick = function oneClick(data, marked, board) {
   found = false;
   for (j = 0, len = ACES.length; j < len; j++) {
     heap = ACES[j];
-    if (legalMove(board, marked, heap)) {
+    if (legalMove(board, marked[0], heap)) {
       if (sharp) {
-        makeMove(board, marked, heap, true);
+        makeMove(board, marked[0], heap, true);
       }
       found = true;
       return heap;
@@ -763,11 +764,11 @@ oneClick = function oneClick(data, marked, board) {
     for (l = 0, len1 = HEAPS.length; l < len1; l++) {
       heap = HEAPS[l];
       if (board[heap].length === 0) {
-        if (indexOf.call(PANEL, marked) >= 0 || calcAntal(board[marked]) > 1) {
+        if ((ref = marked[0], indexOf.call(PANEL, ref) >= 0) || calcAntal(board[marked[0]]) > 1) {
           holes.push(heap);
         }
       } else {
-        if (legalMove(board, marked, heap)) {
+        if (legalMove(board, marked[0], heap)) {
           alternativeDsts.push(heap);
         }
       }
@@ -778,13 +779,13 @@ oneClick = function oneClick(data, marked, board) {
     if (alternativeDsts.length > 0) {
       heap = alternativeDsts[data.counter % alternativeDsts.length];
       if (sharp) {
-        makeMove(board, marked, heap, true);
+        makeMove(board, marked[0], heap, true);
       }
       data.lastMarked = marked;
       return heap;
     }
   }
-  return marked; // no Move can happen
+  return marked[0];
 };
 
 // assert1.jpg
@@ -793,127 +794,139 @@ b1 = readBoard("cA|hA|sA|dA|h5|c3|s65|c2 d5||s3|d2 h6 d4|d3 h4|h2|c5|c4|h3|c6|s4
 assert(11, oneClick({
   lastMarked: 0,
   counter: 0
-}, 4, b1)); // hj5 to hj4
+}, [4, 0], b1)); // hj5 to hj4
 
 assert(5, oneClick({
   lastMarked: 0,
   counter: 0
-}, 5, b1)); // kl3 no move
+}, [5, 0], b1)); // kl3 no move
 
 assert(8, oneClick({
   lastMarked: 0,
   counter: 0
-}, 6, b1)); // sp5 to hole
+}, [6, 1], b1)); // sp5 to hole
 
 assert(10, oneClick({
   lastMarked: 0,
   counter: 0
-}, 7, b1)); // ru5 to ru4
+}, [7, 1], b1)); // ru5 to ru4
 
 assert(8, oneClick({
-  lastMarked: 7,
+  lastMarked: [7, 1],
   counter: 0
-}, 7, b1)); // ru5 to hole
+}, [7, 1], b1)); // ru5 to hole
 
 assert(8, oneClick({
   lastMarked: 0,
   counter: 0
-}, 8, b1)); // hole click
+}, [8, -1], b1)); // hole click
 
 assert(9, oneClick({
   lastMarked: 0,
   counter: 0
-}, 9, b1)); // sp3 no move
+}, [9, 0], b1)); // sp3 no move
 
 assert(7, oneClick({
   lastMarked: 0,
   counter: 0
-}, 10, b1)); // ru4 to ru5
+}, [10, 2], b1)); // ru4 to ru5
 
 assert(8, oneClick({
-  lastMarked: 10,
+  lastMarked: [10, 2],
   counter: 0
-}, 10, b1)); // ru4 to hole
+}, [10, 2], b1)); // ru4 to hole
 
 assert(7, oneClick({
-  lastMarked: 10,
+  lastMarked: [10, 2],
   counter: 1
-}, 10, b1)); // ru4 to ru5
+}, [10, 2], b1)); // ru4 to ru5
+
+b1a = readBoard("cA|hA|sA|dA|h5|c3|s65|c2 d54||s3|d2 h6|d3 h4|h2|c5|c4|h3|c6|s4|s2|d6");
+
+assert(4, oneClick({
+  lastMarked: [10, 2],
+  counter: 0
+}, [10, 1], b1a)); // hj6 to hj5
+
+assert(8, oneClick({
+  lastMarked: [10, 1],
+  counter: 0
+}, [10, 1], b1a)); // hj6 to hole
 
 assert(4, oneClick({
   lastMarked: 0,
   counter: 0
-}, 11, b1)); // hj4 to hj5
+}, [11, 1], b1)); // hj4 to hj5
 
 assert(8, oneClick({
-  lastMarked: 11,
+  lastMarked: [11, 1],
   counter: 0
-}, 11, b1)); // hj4 to hole
+}, [11, 1], b1)); // hj4 to hole xxx
 
 assert(1, oneClick({
   lastMarked: 0,
   counter: 0
-}, 12, b1)); // hj2 to A
+}, [12, 0], b1)); // hj2 to A
 
 assert(8, oneClick({
   lastMarked: 0,
   counter: 0
-}, 13, b1)); // kl5 to hole
+}, [13, 0], b1)); // kl5 to hole
 
 assert(5, oneClick({
   lastMarked: 0,
   counter: 0
-}, 14, b1)); // kl4 to kl3
+}, [14, 0], b1)); // kl4 to kl3
 
 assert(8, oneClick({
-  lastMarked: 14,
+  lastMarked: [14, 0],
   counter: 0
-}, 14, b1)); // kl4 to hole
+}, [14, 0], b1)); // kl4 to hole
 
 assert(11, oneClick({
   lastMarked: 0,
   counter: 0
-}, 15, b1)); // hj3 to hj4
+}, [15, 0], b1)); // hj3 to hj4
 
 assert(8, oneClick({
-  lastMarked: 15,
+  lastMarked: [15, 0],
   counter: 0
-}, 15, b1)); // hj3 to hole
+}, [15, 0], b1)); // hj3 to hole
 
 assert(8, oneClick({
   lastMarked: 0,
   counter: 0
-}, 16, b1)); // kl6 to hole
+}, [16, 0], b1)); // kl6 to hole
 
 assert(6, oneClick({
   lastMarked: 0,
   counter: 0
-}, 17, b1)); // sp4 to sp5
+}, [17, 0], b1)); // sp4 to sp5
 
 assert(9, oneClick({
-  lastMarked: 17,
+  lastMarked: [17, 0],
   counter: 0
-}, 17, b1)); // sp4 to sp3
+}, [17, 0], b1)); // sp4 to sp3
 
 assert(8, oneClick({
-  lastMarked: 17,
+  lastMarked: [17, 0],
   counter: 1
-}, 17, b1)); // sp4 to hole
+}, [17, 0], b1)); // sp4 to hole
 
 assert(2, oneClick({
   lastMarked: 0,
   counter: 0
-}, 18, b1)); // sp2 to A
+}, [18, 0], b1)); // sp2 to A
 
 assert(7, oneClick({
   lastMarked: 0,
   counter: 0
-}, 19, b1)); // ru6 to ru5
+}, [19, 0], b1)); // ru6 to ru5
 
 assert(8, oneClick({
-  lastMarked: 19,
+  lastMarked: [19, 0],
   counter: 0
-}, 19, b1)); // ru6 to hole
+}, [19, 0], b1)); // ru6 to hole
 
 
 // assert2.jpg
@@ -941,7 +954,7 @@ mousePressed = function mousePressed() {
       display(board);
       return;
     }
-    marked = mx + (my >= 3 ? 12 : 4);
+    marked = [mx + (my >= 3 ? 12 : 4), my];
     heap = oneClick(oneClickData, marked, board, true);
     if (msg === '' && 4 * N === countAceCards(board)) {
       msg = Math.floor((millis() - start) / 1000) + " s";
