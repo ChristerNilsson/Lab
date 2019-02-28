3bbbd75848132e5d9f79b089f2fdfb924bf5d328
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-02-28 21:34:55 +0100

    x

--- 2019/023-hashcode/023.py
+++ 2019/023-hashcode/023.py
@@ -4,6 +4,8 @@
 # V 2 garden selfie
 # H 2 garden cat
 
+import random
+
 class Photo:
 	def __init__(self,index,orientation,tags):
 		self.index = index
@@ -31,18 +33,23 @@ class Slide:
 class Problem:
 	def __init__(self,letter):
 		self.letter = letter
+		self.vphotos = []
+		self.hphotos = []
 		self.photos = []
 		self.slides = []
 		with open(letter + '.txt') as f:
 			head = f.readline()
 			n = int(head)
+			print(n)
 			for i in range(n):
 				arr = f.readline().strip().split(' ')
 				index = i
 				orientation = arr[0]
 				tags = arr[2:]
 				photo = Photo(index,orientation,tags)
-				self.photos.append(photo)
+				if orientation == 'V': self.vphotos.append(photo)
+				if orientation == 'H': self.hphotos.append(photo)
+
 	def score(self,slide1,slide2):
 		a = slide1.set.intersection(slide2.set)
 		b = slide1.set.difference(slide2.set)
@@ -50,37 +57,65 @@ class Problem:
 		return min(len(a),len(b),len(c))
 
 	def combine(self):
-		slide = Slide()
-		slide.add(self.photos[0])
-		self.slides.append(slide)
-		slide = Slide()
-		slide.add(self.photos[3])
-		self.slides.append(slide)
-		slide = Slide()
-		slide.add(self.photos[1])
-		slide.add(self.photos[2])
-		self.slides.append(slide)
+		print('vphotos',len(self.vphotos))
+		for i in range(len(self.vphotos)//2):
+			slide = Slide()
+			slide.add(self.vphotos[2*i])
+			slide.add(self.vphotos[2*i+1])
+			self.slides.append(slide)
+		print('hphotos',len(self.hphotos))
+		for i in range(len(self.hphotos)):
+			slide = Slide()
+			slide.add(self.hphotos[i])
+			self.slides.append(slide)
+
+		self.cands = []
+		print(len(self.slides))
+		for i in range(len(self.slides)):
+			self.cands.append(i)
+
+		total = 0
+		for i in range(1000):
+			r1 = random.randint(1,len(self.slides)-2)
+			a = self.cands[r1-1]
+			b = self.cands[r1]
+			c = self.cands[r1+1]
+			r2 = random.randint(1,len(self.slides)-2)
+			d = self.cands[r2-1]
+			e = self.cands[r2]
+			f = self.cands[r2+1]
+			ab = self.score(self.slides[a],self.slides[b])
+			bc = self.score(self.slides[b],self.slides[c])
+			de = self.score(self.slides[d],self.slides[e])
+			ef = self.score(self.slides[e],self.slides[f])
+
+			ae = self.score(self.slides[a],self.slides[e])
+			ec = self.score(self.slides[e],self.slides[c])
+			db = self.score(self.slides[d],self.slides[b])
+			bf = self.score(self.slides[b],self.slides[f])
+
+			old = ab+bc+de+ef
+			new = ae+ec+db+bf
+			if new > old:
+				total += new - old
+				print('better', new-old,r1,r2,total)
+				self.cands[r1],self.cands[r2] = self.cands[r2],self.cands[r1]
 
 	def write(self):
 		with open(self.letter + '.out', 'w') as f:
-			print(f"{len(self.slides)}", file=f)
-			for slide in self.slides:
+			print(f"{len(self.cands)}", file=f)
+			for cand in self.cands:
+				slide = self.slides[cand]
 				if len(slide.photos)==1:
 					print(f"{slide.photos[0].index}", file=f)
 				else:
 					print(f"{slide.photos[0].index} {slide.photos[1].index}", file=f)
 
-problem = Problem('a')
-for photo in problem.photos:
-	print(photo)
+problem = Problem('e')
+print(len(problem.vphotos))
+print(len(problem.hphotos))
 
 problem.combine()
-
-for i in range(len(problem.slides)-1):
-	slide1 = problem.slides[i]
-	slide2 = problem.slides[i+1]
-	print(problem.score(slide1,slide2))
-
 problem.write()
 
 
