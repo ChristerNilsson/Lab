e6684209498cf6d297639818ba0c732b435b0264
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-03-18 16:52:16 +0100

    x

--- 2019/029-ChemicalCalculator/README.md
+++ 2019/029-ChemicalCalculator/README.md
@@ -1,15 +1,14 @@
-# Nim
+# Chemical Calculator
 
-## Detta gör du när du skapar ett nytt Nim-projekt
+# http://www.rosettacode.org/wiki/Chemical_Calculator
 
-* Använd createProject för att skapa en kopia av 000-Nim
-* Byt namn på katalogen NewNim. T ex till 001-projektnamn.
-
-## Detta gör du när du utvecklar
-
-* Gå in i katalogen och starta startProject
-  * sublime startas.
-  	* Stäng eventuellt befintligt projekt med högerklick|Remove Folder from Project
-  	* Stäng eventuella befintliga filer med File|Close All Files
-* Ändra i sketch.nim.
-* Ge kommandot compile sketch
+* assert 1.008 == molar_mass('H')
+* assert 2.016 == molar_mass('H2')
+* assert 18.015 == molar_mass('H2O')
+* assert 34.014 == molar_mass('H2O2')
+* assert 34.014 == molar_mass('(HO)2')
+* assert 142.03553856000002 == molar_mass('Na2SO4')
+* assert 84.162 == molar_mass('C6H12')
+* assert 186.29499999999996 == molar_mass('COOH(C(CH3)2)3CH3') # See details above.
+* assert 176.124 == molar_mass('C6H4O2(OH)4') # Vitamin C
+* assert 386.664 == molar_mass('C27H46O') # Cholesterol
--- 2019/029-ChemicalCalculator/chemical_calculator.py
+++ 2019/029-ChemicalCalculator/chemical_calculator.py
@@ -1,6 +1,6 @@
 from atomic_mass import ATOMIC_MASS
 
-def parse(s): # 'H2O' => H 2 O
+def parse(s): # 'H2O' => H 2 O and 'A1A'
 	result = []
 	pattern = ''
 	i = 0
@@ -34,23 +34,23 @@ def pass1(m1,m2): # H 2 O => H * 2 + O
 		'+*+=',  # A = Atom
 		'+x+=',  # 1 = Count
 		'=x=x',  # (
-		'+*+=']  # )
+		'+*+='   # )
+	]
+	operation = {
+		'+': lambda a : [a, '+'],
+		'*': lambda a : [a, '*'],
+		'x': lambda a : [],
+		'=': lambda a : [a]
+	}
 
 	result = []
-
-	add = lambda a, b: [a, '+']
-	mul = lambda a, b: [a, '*']
-	error = lambda a, b: []
-	nop = lambda a, b: [a]
-	operation = {'+': add, '*': mul, 'x': error, '=': nop}
-
 	for i in range(len(m1)-1):
 		ch0 = m2[i]
 		ch1 = m2[i+1]
 		i0 = SYMBOLS.index(ch0)
 		i1 = SYMBOLS.index(ch1)
 		op = matrix[i0][i1]
-		result += operation[op](m1[i],m1[i+1])
+		result += operation[op](m1[i])
 	result.append(m1[-1])
 	return result
 
@@ -108,11 +108,19 @@ def pass3(rpn): # H 2 * O + => { H:2, O:1 }
 def pass4(atoms): # { H:2, O:1 } => 18.015
 	return sum([ATOMIC_MASS[key] * atoms[key] for key in atoms])
 
-def molar_mass(molecule):
+def molar_mass(molecule,debug = False):
 	atom_list,pattern = parse(molecule)
 	infix = pass1(atom_list,pattern)
 	rpn = pass2(infix)
 	atoms = pass3(rpn)
+	if debug:
+		print('molecule',molecule)
+		print('atom_list',atom_list)
+		print('pattern',pattern)
+		print('infix',infix)
+		print('rpn',rpn)
+		print('atoms',atoms)
+	return pass4(atoms)
 
 	# assert 'COOH(C(CH3)2)3CH3' == molecule
 	# assert 'C O O H ( C ( C H 3 ) 2 ) 3 C H 3' == ' '.join(atom_list)
@@ -120,11 +128,15 @@ def molar_mass(molecule):
 	# assert 'C + O + O + H + ( C + ( C + H * 3 ) * 2 ) * 3 + C + H * 3' == (' '.join(infix))
 	# assert 'C O O H C C H 3 * + 2 * + 3 * C H 3 * + + + + + +' == ' '.join(rpn)
 	# assert {'C': 11, 'O': 2, 'H': 22} == atoms
-	return pass4(atoms)
 
 assert 1.008 == molar_mass('H')
 assert 2.016 == molar_mass('H2')
 assert 18.015 == molar_mass('H2O')
+assert 34.014 == molar_mass('H2O2')
+assert 34.014 == molar_mass('(HO)2')
 assert 142.03553856000002 == molar_mass('Na2SO4')
 assert 84.162 == molar_mass('C6H12')
-assert 186.29499999999996 == molar_mass('COOH(C(CH3)2)3CH3')
+assert 186.29499999999996 == molar_mass('COOH(C(CH3)2)3CH3') # See details above.
+assert 176.124 == molar_mass('C6H4O2(OH)4') # Vitamin C
+assert 386.664 == molar_mass('C27H46O') # Cholesterol
+
