7ad2a21f4a3db4ac9666f511d54ab7e967782afd
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-09-08 21:41:58 +0200

    x

---
+++ 2019/072-GuessMyNumber-Readline-Exercise/README.md
@@ -0,0 +1,12 @@
+# GuessMyNumber readline-sync
+
+Using readlineSync there is no need for saving the state.
+This makes the program a lot shorter and easier to use!
+Almost all programming languages has a similar construct.
+
+## Start 
+
+* node index
+
+[readline-sync](https://www.npmjs.com/package/readline-sync)
+
---
+++ 2019/072-GuessMyNumber-Readline-Exercise/index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const {question} = require('readline-sync')
+
+let guess = {}
+
+const reset = (level) => {
+	if (level < 2) level = 2
+	guess.level = level
+	guess.low = 1
+	guess.high = 2**guess.level - 1
+	guess.history = []
+	guess.secret = Math.ceil(guess.high * Math.random()) 
+}
+
+reset(2) 
+
+//     Secret = 82 
+//     low   nr    high    Next Interval
+//     1     64    127  => 65-127
+//     65    96    127  => 65-95
+//     65    80     95
+
+while (true) {
+	const nr = JSON.parse(question(`${guess.low}-${guess.high} > `))
+	guess.history.push(nr)
+
+	if (nr == 0) break
+	if (nr < guess.secret) guess.low = nr + 1
+	if (nr > guess.secret) guess.high = nr - 1 
+	if (nr == guess.secret) {
+		console.log('Correct!')
+		if (guess.history.length > guess.level) {
+			reset(guess.level-1)
+		} else {
+			reset(guess.level+1)
+		}
+	}
+}
