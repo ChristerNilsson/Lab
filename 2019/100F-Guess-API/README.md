ab9b31411584ac966bf83fe7d9908abfd52aae5b
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-09-22 21:39:19 +0200

    x

---
+++ 2019/100F-Guess-API/README.md
@@ -0,0 +1,12 @@
+# Guess My Number
+
+Using a web server
+
+# start
+
+nodemon
+
+# Commands
+
+localhost:3000       Creates a new hidden number
+localhost:3000/2     Guesses the hidden number
---
+++ 2019/100F-Guess-API/src/server.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const http = require('http')
+const Game = require ('./game')
+
+function createServer() {
+	const game = new Game(2)
+  return http.createServer((req, res) => {
+		const arr = req.url.split('/')
+		const cmd = arr[1]
+		if (cmd == 'favicon.ico') return
+		if (cmd == '') game.init(2)
+		if (cmd != '') game.action(cmd)
+		res.end(`${game.low}-${game.high}` + '\n')
+	})
+}
+
+module.exports = createServer
