dc754170643a2cce9889040298335cc402a64360
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-10-15 19:35:55 +0200

    x

---
+++ 2019/105-OrganizerPage-Svelte/src/EditPage.svelte
@@ -0,0 +1,19 @@
+<script>
+	import {createEventDispatcher} from 'svelte'
+	const dispatch = createEventDispatcher()
+
+	export let lines
+
+	const save = () => dispatch('changepage',lines)
+
+</script>
+
+<style>
+	textarea,button {
+		font-size: 30px;
+		line-height: 1.2;
+	}
+</style>
+
+<button on:click={save}>Save</button>
+<textarea rows=70 bind:value={lines}></textarea>
---
+++ 2019/105-OrganizerPage-Svelte/src/FindPage.svelte
@@ -0,0 +1,26 @@
+<script>
+	import {createEventDispatcher} from 'svelte'
+	const dispatch = createEventDispatcher()
+
+	export let pattern
+	export let lines 
+
+	$: showLines = lines.split('\n').filter((line) => line.toLowerCase().includes(pattern))	
+
+	const edit = () => dispatch('changepage',lines)
+</script> 
+
+<style>
+	ul,input,button {
+		font-size: 30px;
+		line-height: 1.2;
+	}
+</style>
+
+<button on:click={edit}>Edit</button>
+<input type="text" placeholder='Search' bind:value={pattern}>
+<ul>
+	{#each showLines as line}
+		<li>{line}</li>
+	{/each}
+</ul>
