818c6c7793636723432f110e44f8cdf977a9d330
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-10-17 02:31:16 +0200

    x

---
+++ 2019/104-Svelte-Playground/create.bat
@@ -0,0 +1,5 @@
+rem @echo off
+rem .\create 099
+
+xcopy arkiv\src000\*.* arkiv\src%1\
+xcopy arkiv\public000\*.* arkiv\public%1\
---
+++ 2019/104-Svelte-Playground/destroy.bat
@@ -0,0 +1,5 @@
+@echo off
+rem .\destroy 099
+
+rd arkiv\src%1 /s /q
+rd arkiv\public%1 /s /q 
