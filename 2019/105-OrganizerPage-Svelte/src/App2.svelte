4797b6b92327111c334ecbc4e582f40f090ae16c
Christer Nilsson <janchrister.nilsson@gmail.com>
2025-09-11 12:49:54 +0200

    Delete 2019/105-OrganizerPage-Svelte directory

--- 2019/105-OrganizerPage-Svelte/src/App2.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	document.title = 'App2'
-</script> 
-
-<h1>App2</h1>
--- 2019/105-OrganizerPage-Svelte/src/main.js
+++
@@ -1,9 +0,0 @@
-import App from './App.svelte';
-//import App from './App2.svelte';
-
-const app = new App({
-	target: document.body,
-	props: {}
-});
-
-export default app;
